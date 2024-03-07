57c2d03a4e1d0205c9d0da1feb49abab033a8f54
Steven John <stevenjpx2@gmail.com>
2024-03-07 21:25:46 +0530

    refactor!: move all dependencies to dev, use nuxt/fonts instead of fontaine

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,16 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  devtools: { enable: true },
   extends: ["nuxt-seo-kit"],
   modules: [
     "@nuxtjs/critters",
     "nuxt-icon",
     "@vite-pwa/nuxt",
     "@nuxtjs/html-validator",
-    "@nuxtjs/fontaine",
     "@vueuse/nuxt",
     "nuxt-delay-hydration",
     "@fdcn/nugget",
     "@nuxtjs/tailwindcss",
     "@nuxt/image",
+    "@nuxt/fonts",
   ],
 });
