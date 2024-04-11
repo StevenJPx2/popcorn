c3d907ad2be3afc08455c9182e91a8222aa7be86
Steven John <stevenjpx2@gmail.com>
2024-04-11 16:50:33 +0530

    feat!: remove nuxt-seo-kit for @nuxtjs/seo

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enable: true },
-  extends: ["nuxt-seo-kit"],
+  extends: ["@nuxtjs/seo"],
   modules: [
     "nuxt-icon",
     "@vite-pwa/nuxt",
